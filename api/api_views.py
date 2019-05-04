6bb66e1a5d7636750ef9ecd9863926d5a40a379b
dmitrii <sokolovdp@gmail.com>
2019-05-04 13:25:20 +0300

    add username from session

--- api/api_views.py
+++ api/api_views.py
@@ -1,13 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from main_app.logger import django_logger
 
 
 class Test(APIView):
-    permission_classes = []
-    authentication_classes = []
+    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-        django_logger.info('Test API')
-        return Response({"test": 12345})
+        user = request.user.username
+        django_logger.info(f'Test API user: {user}')
+        return Response({"test user": user})
--- otus_django_project/settings.py
+++ otus_django_project/settings.py
@@ -166,7 +166,7 @@ REST_FRAMEWORK = {
     #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     # ]
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     )
 }
