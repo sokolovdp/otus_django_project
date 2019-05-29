1c9f48cf4f6a750d7789ad00cee28e1f3bc51ae1
dmitrii <sokolovdp@gmail.com>
2019-05-29 16:17:16 +0300

    add items_list path, update html templates

--- main_app/main_urls.py
+++ main_app/main_urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('register/', main_views.register, name='register'),
     path('login/', main_views.user_login, name='user_login'),
     path('logout/', main_views.user_logout, name='user_logout'),
+    path('items_list/', main_views.items_list, name='items_list'),
 ]
